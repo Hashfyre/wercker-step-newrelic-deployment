69a967010f8de576f342e8f9703dd4fd463665dd
Rafael Verger <rafael@rafaelverger.com.br>
2014-08-11 00:42:43 -0300

    Set NewRelic api url as bash variable

--- run.sh
+++ run.sh
@@ -12,4 +12,8 @@ if [ ! -n "$WERCKER_NEWRELIC_DEPLOYMENT_REVISION" ]; then
   export WERCKER_NEWRELIC_DEPLOYMENT_REVISION="$WERCKER_GIT_BRANCH/${WERCKER_GIT_COMMIT:0:10} by $WERCKER_STARTED_BY"
 fi
 
-curl -H "x-api-key:${WERCKER_NEWRELIC_DEPLOYMENT_API_KEY}" -d "deployment[app_name]=${WERCKER_NEWRELIC_DEPLOYMENT_APP_NAME}" -d "deployment[revision]=${WERCKER_NEWRELIC_DEPLOYMENT_REVISION}" https://api.newrelic.com/deployments.xml
+NEWRELIC_API="https://api.newrelic.com"
+NR_API_DEPLOYMENTS="$NEWRELIC_API/deployments.xml"
+
+curl -H "x-api-key:${WERCKER_NEWRELIC_DEPLOYMENT_API_KEY}" -d "deployment[app_name]=${WERCKER_NEWRELIC_DEPLOYMENT_APP_NAME}" -d "deployment[revision]=${WERCKER_NEWRELIC_DEPLOYMENT_REVISION}" $NR_API_DEPLOYMENTS
+
